5eb23f66516745b8c9044a27ce65ff6ca762ba4e
Kevin Chang <kevin@megalive.com.tw>
2018-10-18 18:02:43 +0800

    [Update] Test okhttp request

--- app/src/main/java/app/kevin/com/androidrequestpractice/MainActivity.java
+++ app/src/main/java/app/kevin/com/androidrequestpractice/MainActivity.java
@@ -2,12 +2,43 @@ package app.kevin.com.androidrequestpractice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
+
+    String TAG = "http";
+    OkHttpClient _client = new OkHttpClient().newBuilder().build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Request request = new Request.Builder()
+                .url("http://192.168.100.51:3000/posts")
+                .build();
+
+        Call call = _client.newCall(request);
+
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String result = response.body().string();
+                Log.d(TAG, "onResponse: " + result);
+            }
+        });
     }
 }
